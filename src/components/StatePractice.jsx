fd1a455ed41eca61c2a312a560171e2ebaf234af
dral123456 <marcelinoarldrich@gmail.com>
2025-04-28 21:13:42 +0800

    map

--- src/components/StatePractice.jsx
+++ src/components/StatePractice.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import map from "../media/Labelled_map_of_the_Philippines_-_Provinces_and_Regions.png"
 
 const StatePractice = () => {
   return (
     <div>
       <h1>Good evening guys!!! We are pleased to work with you!!!</h1>
+      <img src = "map"/>
     </div>
   )
 }
